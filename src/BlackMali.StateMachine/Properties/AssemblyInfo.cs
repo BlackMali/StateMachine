b812b237a626bc94abe72221606200d6bad0cf0a
black_mali <black_mali@yahoo.com>
2022-10-25 17:05:36 +0200

    AssemblyVersion without VersionAttributes

--- src/BlackMali.StateMachine/Properties/AssemblyInfo.cs
+++ src/BlackMali.StateMachine/Properties/AssemblyInfo.cs
@@ -18,9 +18,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("BlackMali.StateMachine")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyDescription("Simple state machine with DI and async support")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.0.1")]
-[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: InternalsVisibleTo("BlackMali.StateMachine.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a927b32b1f12bcca6b00b34567624152978241d2b0e3c8cb6019187f2d5f83eca6e2fd7424153f2c098950a724fe5a2aa834eeded6369ef91df12aa4a555c7d16efcef659028458bb5df715763839a218ad621d5c0b31c1a6332e679d82984a94c6fb81ea4523c633b88cf9995bdbcb1576b959c990e1846cd0a31134f7d72bc")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004­800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225­ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753c­c297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46a­d2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e9248­4cf7045cc7")]
 
